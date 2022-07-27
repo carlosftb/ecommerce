ef6284ac93a64707ffa0e0fedc906f61fbbbfa26
Carlos Silva <carlos.david.ftb@hotmail.com>
2022-07-27 15:29:15 -0300

    tirando duvidas sobre erro

--- index.php
+++ index.php
@@ -52,9 +52,81 @@ $app->post('/admin/login', function() {
 $app->get('/admin/logout', function()
 {
 	User::logout();
+
 	header("Location: /admin/login");
 	exit;
 
+});
+
+$app->get("/admin/users", function() 
+{
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+});
+
+$app->get("/admin/users/create", function() 
+{
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser)
+{
+	User::verifyLogin();
+});
+
+$app->get("/admin/users/:iduser", function($iduser) 
+{
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update");
+});
+
+$app->post("/admin/users/create", function()
+{
+	User::verifyLogin();
+   
+	$user = new User();
+   
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+   
+	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+   
+		"cost"=>12
+   
+	]);
+   
+	$user->setData($_POST);
+   
+	$user->save();
+   
+	header("Location: /admin/users");
+	exit;
+
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser)
+{
+	User::verifyLogin();
+
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser)
+{
+	User::verifyLogin();
 });
 
 $app->run();
